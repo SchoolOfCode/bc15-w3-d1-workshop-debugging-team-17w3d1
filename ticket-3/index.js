12b89f09f0d2966c62e71832aca890a91dae5c73
puiuioanadiana <137444855+puiuioanadiana@users.noreply.github.com>
2023-09-18 16:20:09 +0100

    16:20pm

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -2,11 +2,16 @@ function countNumberOfNinesInNumber(number) {
   let count = 0;
 
   for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
+    const digit = number.charAt(i);
+   
     if ("9" === digit) {
       count++;
     }
   }
 
   return count;
+  console.log(count);
 }
+
+console.log(countNumberOfNinesInNumber("1115.1257846"));
+
