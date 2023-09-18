f93591a6368b06d9fc911c1703e8883dcbe2409c
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-09-18 16:48:51 +0100

    ticket 5 complete

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -19,10 +19,11 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
-  const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
-  totalCost += costForItem;
+for (let i = 0; i < shoppingCart.length; i++) {
+const item = shoppingCart[i];
+ const costForItem = item.price * item["quantity"];
+ totalCost += costForItem;
+
 }
 
 const formatter = new Intl.NumberFormat("en-GB", {
